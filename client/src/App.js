a0a3dbad4c905aa9166e09c8f5e1c38cb6258d0b
Mohamed El-Baz <albaz2000eg@gmail.com>
2019-04-01 16:54:52 +0200

    setup connection client/server

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,44 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import gql from 'graphql-tag'
+import { graphql } from 'react-apollo'
+
+const USERS_QUERY = gql`
+  query users {
+    id
+    name
+  }
+`
 
 class App extends Component {
   render() {
+    // TODO:
+    if (this.props.data.loading) {
+      return <div>Loading</div>
+    }
+
     return (
       <div className="App">
         <header className="App-header">
-          <p>
-            Hello World
-          </p>
+          <p>Hello World</p>
         </header>
+
+        {/* TODO */}
+        <div className="App-intro">
+          <ul>
+            {this.props.data.users &&
+              this.props.data.users.map((user, index) => {
+                return (
+                  <li key={index}>
+                    {user.name}
+                  </li>
+                )
+              })}
+          </ul>
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default graphql(USERS_QUERY)(App)
