6eda4a8a9d9fc0acc9952161902769dd8dc43c14
Mohamed Elbaz <albaz2000eg@gmail.com>
2019-04-17 03:26:36 +0200

    add Lab List component

--- client/src/pages/labs/Labs.js
+++ client/src/pages/labs/Labs.js
@@ -1,23 +1,15 @@
 import React from 'react'
 import { Route, NavLink } from 'react-router-dom'
-import { Query } from 'react-apollo'
 import Create from './create'
 import Lab from './lab';
 import Edit from './edit'
-import getAllLabs from './getAllLabs.gql';
+import List from './list';
 
 const Labs = () => {
   return (
     <div>
       <NavLink to="/labs/create">Create New Lab</NavLink>
-      <Query query={getAllLabs}>
-        {({ data }) => {
-          if (!data || !data.labs) return <div>Loading...</div>
-          return data.labs.map(lab => {
-            return (<div key={lab.id}><div>{lab.id}: {lab.name}</div><NavLink to={`/labs/${lab.id}`}>Edit</NavLink></div>)
-          })
-        }}
-      </Query>
+      <Route exact path="/labs" component={List} />
       <Route path="/labs/create" component={Create} />
       <Route path="/labs/:id" component={Lab} />
       <Route path="/labs/:id/edit" component={Edit} />
---
+++ client/src/pages/labs/list/List.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { Query } from 'react-apollo'
+import getAllLabs from './getAllLabs.gql';
+
+const List = () => {
+    return (
+        <Query query={getAllLabs}>
+            {({ data }) => {
+                if (!data || !data.labs) return <div>Loading...</div>
+                return data.labs.map(lab => {
+                    return (<div key={lab.id}><div>{lab.id}: {lab.name}</div><NavLink to={`/labs/${lab.id}`}>Edit</NavLink></div>)
+                })
+            }}
+        </Query>
+    )
+}
+
+export default List
